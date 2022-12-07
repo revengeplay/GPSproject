9e599fab559b8ebd27ba1a5c870ea6ac588152f4
revengeplay <revengeplay@naver.com>
2022-12-08 06:35:29 +0900

    first commit

---
+++ src/main/java/com/gps/GPSproject/GpSprojectApplication.java
@@ -0,0 +1,13 @@
+package com.gps.GPSproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpSprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpSprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gps/GPSproject/GpSprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gps.GPSproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpSprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
